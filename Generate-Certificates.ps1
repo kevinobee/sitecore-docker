5f1b3e109b8850629b7cbf9bc7ff2eb02a7c8eab
Raul Jimenez <rauljmz@gmail.com>
2018-08-14 15:36:46 +0100

    Create SSL certificate for XConnect in host

--- Generate-Certificates.ps1
+++ Generate-Certificates.ps1
@@ -1,3 +1,4 @@
-.\Generate-Self-Signed-Certificate.ps1 -dnsName 'xConnect.client' -file '.\Files\xConnect.pfx' -secret 'secret'
+.\Generate-Self-Signed-Certificate.ps1 -dnsName 'xconnect' -file '.\Files\xconnect.pfx' -secret 'secret'
+.\Generate-Self-Signed-Certificate.ps1 -dnsName 'xConnect.client' -file '.\Files\xConnect.client.pfx' -secret 'secret'
 .\Generate-Self-Signed-Certificate.ps1 -dnsName 'solr' -file '.\Files\solr.pfx' -secret 'secret'
 .\Generate-Self-Signed-Certificate.ps1 -dnsName 'sitecore' -file '.\Files\sitecore.pfx' -secret 'secret'
